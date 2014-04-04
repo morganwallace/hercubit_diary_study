887f47392ce58589da087473a366677c38c99538
Morgan Wallace <morgan@ischool.berkeley.edu>
2014-04-03 18:40:59 -0700

    updates to the machine learning code

--- Python viz/machine_learning/saved_data.py
+++ Python viz/machine_learning/saved_data.py
@@ -8,13 +8,16 @@ def get_all(pickled=False, get_columns_only=False, to_csv=False):
 	dir_name=os.path.join('..','saved_animations_and_data')
 	sample_sets=os.listdir(dir_name)
 	for sample_set in sample_sets:
-		if sample_set!= '.DS_Store':
+		if sample_set[0]!= '.':
 			try: csv_file=os.path.join(dir_name,sample_set,os.listdir(os.path.join(dir_name,sample_set))[0])
 			except:pass
-			with open(csv_file) as f:
+			# print csv_file
+			with open(csv_file,'r') as f:
 				if labels==[]:
-					labels=[label.strip()  for label in f.readline().split(",")]
-					
+					d=f.read()
+					d=d[:d.find('\r')]
+					labels=[label.strip()  for label in d.split(",")]
+					# print labels
 					#override the default behavior and return just the labels
 					if get_columns_only==True: 
 						return labels
@@ -37,4 +40,4 @@ def get_all(pickled=False, get_columns_only=False, to_csv=False):
 
 if __name__ == '__main__':
 	# get_all()
-	get_all(pickled=True, to_csv=True)
+	print get_all(get_columns_only=True)
